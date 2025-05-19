a10acbe8b69efe4ad087b3303965dbaeaa6e5d75
alvee <alvee.rucse@gmail.com>
2025-05-19 22:55:57 +0600

    Warning fixed

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,30 +6,14 @@ export default function RootLayout() {
   return (
     <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="login/index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name='chapter' options={{ headerShown: false }} />
         <Stack.Screen name='chapter/learnPage' options={{ title: 'Chapter Page' }} />
         <Stack.Screen name='quiz/quiz' options={({ route }) => ({ 
                                                   title: `Chapter ${route.params.chapterNo} Quiz`,
-            headerTitleStyle: styles.headerTitle,
-            headerStyle: styles.header
                                                 })} />
     </Stack>
   )
 }
- 
-const styles = StyleSheet.create({
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333',
-    textAlign: 'center',
-  },
-  header: {
-    backgroundColor: '#f8f8f8', // Light background color
-    elevation: 4, // Android shadow
-    shadowOpacity: 0.3, // iOS shadow
-    shadowRadius: 4,
-  }
-})
+
+const styles = StyleSheet.create({})
